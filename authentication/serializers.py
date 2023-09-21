083bbd2b5f2cefa749f6472ba85fd021422df2e3
MictovicDev <awaemekamichael@gmail.com>
2023-09-21 18:42:17 +0100

    changes

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -3,7 +3,6 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework import serializers
 from django.contrib.auth.hashers import make_password
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-import s
 from rest_framework import serializers,response,status
 from . import countries
 
