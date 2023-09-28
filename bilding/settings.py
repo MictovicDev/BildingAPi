933c9bb85c09a708fa4ae213fba1b82e5c6e43a9
MictovicDev <awaemekamichael@gmail.com>
2023-09-28 12:40:00 +0100

    token

--- bilding/settings.py
+++ bilding/settings.py
@@ -209,7 +209,7 @@ CORS_ALLOWED_ORIGINS = [
 ...
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=90),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
