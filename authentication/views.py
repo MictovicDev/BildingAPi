e192da3533a575f67be0e372132d66299c073111
MictovicDev <awaemekamichael@gmail.com>
2023-10-13 21:17:16 +0100

    done

--- authentication/views.py
+++ authentication/views.py
@@ -136,7 +136,7 @@ class SupplierListCreateView(generics.ListCreateAPIView):
             useremail = user.email
             token = RefreshToken.for_user(user)
             user.token = token
-            email.send_linkmail(first_name, token,email)
+            useremail.send_linkmail(first_name, token,email)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -153,7 +153,7 @@ class WorkerListCreateView(generics.ListCreateAPIView):
             token = RefreshToken.for_user(user)
             user.token = token
             user.save()
-            email.send_linkmail(first_name, token,email)
+            useremail.send_linkmail(first_name, token,email)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
