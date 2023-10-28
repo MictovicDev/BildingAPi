4f1969c024a118767618d688bf376fb742911714
MictovicDev <awaemekamichael@gmail.com>
2023-10-28 12:02:02 +0100

    done

--- bilding/settings.py
+++ bilding/settings.py
@@ -265,7 +265,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bilding.settings')
 
 
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
@@ -274,10 +274,10 @@ EMAIL_HOST_PASSWORD = 'eotwybwflcjxpdbv'
 DEFAULT_FROM_USER = 'bildingconstruction@gmail.com'
 SERVER_EMAIL =  'bildingconstruction@gmail.com' 
 # else:
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-CELERY_BROKER_URL = 'redis://default:HX0Z9kWQBAgfrRQt1hPk38DeDR7aW1no@redis-10255.c309.us-east-2-1.ec2.cloud.redislabs.com:10255'  # Use your Redis server URL
-CELERY_RESULT_BACKEND = 'redis://default:HX0Z9kWQBAgfrRQt1hPk38DeDR7aW1no@redis-10255.c309.us-east-2-1.ec2.cloud.redislabs.com:10255'
+# CELERY_BROKER_URL = 'redis://default:HX0Z9kWQBAgfrRQt1hPk38DeDR7aW1no@redis-10255.c309.us-east-2-1.ec2.cloud.redislabs.com:10255'  # Use your Redis server URL
+# CELERY_RESULT_BACKEND = 'redis://default:HX0Z9kWQBAgfrRQt1hPk38DeDR7aW1no@redis-10255.c309.us-east-2-1.ec2.cloud.redislabs.com:10255'
 
 
 # CELERY_BROKER_URL = 'memory://localhost/'
